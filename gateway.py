e2d7228a8bbe80fcc363de381e1cffa10de78082
mohamedblueprintrak-design <mohamed.blueprintrak@gmail.com>
2026-03-08 13:14:16 +0400

    Update gateway.py

--- gateway.py
+++ gateway.py
@@ -38,7 +38,6 @@ st.markdown(pwa_html, unsafe_allow_html=True)
 
 BACKEND = "https://blueprint-app-jrwp.onrender.com"
 BACKEND = "https://blueprint-app-jrwp.onrender.com"  # عدّل الرابط حسب خدمتك
- a59f9d4 (تحسينات واجهة PWA وإصلاحات الموبايل)
 
 # تهيئة session state
 if "msgs" not in st.session_state:
